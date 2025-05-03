07fb9597afd9727b7805cbb513322e38ca35f618
mothgharenimish <mothgharenimish0@gmail.com>
2025-05-03 16:28:46 +0530

    Initial Commit

---
+++ Carousel1-SwiftUI/Carousel1_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carousel1_SwiftUIApp.swift
+//  Carousel1-SwiftUI
+//
+//  Created by Nimish Mothghare on 03/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Carousel1_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Carousel1-SwiftUITests/Carousel1_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  Carousel1_SwiftUITests.swift
+//  Carousel1-SwiftUITests
+//
+//  Created by Nimish Mothghare on 03/05/25.
+//
+
+import Testing
+@testable import Carousel1_SwiftUI
+
+struct Carousel1_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
